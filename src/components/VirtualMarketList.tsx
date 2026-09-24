884b69e113e9bc114bb7ab41e08fb4b5494512b3
JAMIE01 <juniorjeremy105@gmail.com>
2026-09-24 09:21:26 +0300

    fix 6

--- src/components/VirtualMarketList.tsx
+++ src/components/VirtualMarketList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { VariableSizeList as List } from "react-window";
+import { FixedSizeList as List } from "react-window";
 import { MarketItem } from "./MarketItem";
 import { useMarkets } from "@/app/hooks/use-markets";
 
