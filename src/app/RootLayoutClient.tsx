8ce03a71f509df9f29dd9e8f4eb6f34c6858267f
JAMIE01 <juniorjeremy105@gmail.com>
2026-09-24 14:14:53 +0300

    fix 7

--- src/app/RootLayoutClient.tsx
+++ src/app/RootLayoutClient.tsx
@@ -5,16 +5,19 @@ import QueryProvider from "./query-provider";
 import SocketIOProvider from "./socketio-provider";
 import { AppErrorBoundary } from "./error-boundary";
 import { LocaleProvider } from "@/lib/locale-context";
+import { NotificationProvider } from "@/components/NotificationProvider";
 
 export default function RootLayoutClient({ children }: { children: ReactNode }) {
   return (
     <AppErrorBoundary>
       <QueryProvider>
-        <SocketIOProvider socketUrl={process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001"}>
-          <LocaleProvider>
-            {children}
-          </LocaleProvider>
-        </SocketIOProvider>
+        <NotificationProvider>
+          <SocketIOProvider socketUrl={process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001"}>
+            <LocaleProvider>
+              {children}
+            </LocaleProvider>
+          </SocketIOProvider>
+        </NotificationProvider>
       </QueryProvider>
     </AppErrorBoundary>
   );
